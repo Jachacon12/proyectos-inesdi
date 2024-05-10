5c6d0a17543a2042f04c9ba36f81e87803a1d2d8
Jachacon12 <jorgech.webdeveloper@gmail.com>
2024-05-10 15:20:14 +0200

    fix: new routes corrected

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -4,8 +4,8 @@ const todoController = require('../controllers/todoController');
 
 router.get('/', todoController.getAllTodos);
 router.post('/', todoController.createTodo);
-router.patch('/todos/:id', todoController.updateTodo);
-router.delete('/todos/:id', todoController.deleteTodo);
-router.put('/todos/:id', todoController.replaceOrCreateTodo);
+router.patch('/:id', todoController.updateTodo);
+router.delete('/:id', todoController.deleteTodo);
+router.put('/:id', todoController.replaceOrCreateTodo);
 
 module.exports = router;
