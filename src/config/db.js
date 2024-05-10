8625dbd2e197e29fcb9bcba6b2e9b11ade88d1cb
Jachacon12 <jorgech.webdeveloper@gmail.com>
2024-05-10 13:08:12 +0200

    feat: config update to work with standalone scripts

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,19 @@
+// src/config/db.js
 const mongoose = require('mongoose');
 
+const dbURI = process.env.MONGO_URL || 'mongodb://mongo:27017/todoapp';
+
 const connectDB = async () => {
-  try {
-    await mongoose.connect('mongodb://mongo:27017/todoapp', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected...');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(dbURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB connected...');
+    } catch (err) {
+        console.error('Could not connect to MongoDB:', err.message);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
