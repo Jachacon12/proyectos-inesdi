0faadaeed87b723e6f0f55b3e021b4b8725cb54a
Jachacon12 <jorgech.webdeveloper@gmail.com>
2024-05-10 12:18:26 +0200

    feat!: new DB configuration added to app

--- src/app.js
+++ src/app.js
@@ -1,22 +1,13 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const Todo = require('./models/todo');
+const connectDB = require('./config/db');
+const todoRoutes = require('./routes/todos');
 
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://mongo:27017/todoapp', { useNewUrlParser: true, useUnifiedTopology: true });
+connectDB();
 
-app.get('/todos', async (req, res) => {
-  const todos = await Todo.find();
-  res.send(todos);
-});
-
-app.post('/todos', async (req, res) => {
-  const todo = new Todo(req.body);
-  await todo.save();
-  res.send(todo);
-});
+app.use('/todos', todoRoutes);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
