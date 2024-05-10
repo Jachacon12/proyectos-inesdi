fad46f3caa1b4a9708b6b94d62cd00314c0e3465
Jachacon12 <jorgech.webdeveloper@gmail.com>
2024-05-10 14:59:46 +0200

    refactor: PORT added as a variable & 3000 set as fallback value

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const todoRoutes = require('./routes/todos');
-
+const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(express.json());
 
-connectDB();
+app.use(express.json());
 
 app.use('/todos', todoRoutes);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+connectDB();
