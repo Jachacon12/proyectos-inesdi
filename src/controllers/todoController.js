1f818bce167581aa87f0f04fa0f7e5c22bc7f497
Jachacon12 <jorgech.webdeveloper@gmail.com>
2024-05-10 14:45:07 +0200

    feat: logic to handle updating a todo item

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -22,3 +22,18 @@ exports.createTodo = async (req, res) => {
     res.status(400).send(error);
   }
 };
+
+// Handle updating a todo
+exports.updateTodo = async (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+  try {
+    const todo = await Todo.findByIdAndUpdate(id, updates, { new: true });
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send(todo);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
