539867fa4832b2e10aecdd9394468bbe40b43aa7
Jachacon12 <jorgech.webdeveloper@gmail.com>
2024-05-10 14:33:49 +0200

    feat: Title set as required

--- src/models/todo.js
+++ src/models/todo.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
 const todoSchema = new mongoose.Schema({
-  title: String,
-  completed: {
-    type: Boolean,
-    default: false
-  }
+  title: { type: String, required: true },
+  completed: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('Todo', todoSchema);
